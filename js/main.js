e3733d1463c9d6f79273349fe4f879f3a63017f5
rhee519 <rhee519@gmail.com>
2021-08-02 21:41:18 +0900

    separate common modules

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-// Header
-const searchElement = document.querySelector(".search");
-const searchInputElement = searchElement.querySelector("input");
-
-searchElement.addEventListener("click", function (event) {
-  searchInputElement.focus();
-});
-
-searchInputElement.addEventListener("focus", function (event) {
-  searchElement.classList.add("focused");
-  searchInputElement.setAttribute("placeholder", "통합검색");
-});
-
-searchInputElement.addEventListener("blur", function (event) {
-  searchElement.classList.remove("focused");
-  searchInputElement.setAttribute("placeholder", "");
-});
-
 // scroll controll
 const badgeElement = document.querySelector("header .badges");
 const toTopElement = document.querySelector("#to-top");
@@ -137,7 +119,3 @@ spyElements.forEach(function (spyElement) {
     .setClassToggle(spyElement, "show")
     .addTo(new ScrollMagic.Controller());
 });
-
-// DATE (YEAR)
-const thisYear = document.querySelector(".this-year");
-thisYear.textContent = new Date().getFullYear();
